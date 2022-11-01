880ef0c3811bd1adfce54ec28d6597dd57b9d81e
Mhayk Whandson <hi@mhayk.com>
2022-11-01 06:14:21 +0000

    feat: create the first rest route scheleton

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
         logger: true
     })
 
+    fastify.get('/pools/count', () => {
+        return { count: 0 }
+    })
+
     await fastify.listen({ port: 3333 })
 }
 
